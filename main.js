2b3bbceb2c2bff181b9ff16e54c32b19ff7b7a2c
Joergen-Lund <jorgen2817@gmail.com>
2021-11-03 12:49:51 +0100

    .

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const screen = document.querySelector('.screen')
+// import Calculator from "./Calculator"
 
-screen.innerHTML = "12345"
+const screen = document.querySelector('.screen')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const equalsButton = document.querySelector('[data-equals]')
+
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => {
+        console.log(numberButton.innerHTML)
+    })
+})
+
+operationButtons.forEach(operationButton => {
+    operationButton.addEventListener('click', () => {
+        console.log(operationButton.value)
+    })
+})
